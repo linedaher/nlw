fa58d80dcc4a9bc13bd9c6ee4b49786919d0b7fa
Aline <aline@proesc.com>
2024-07-08 22:01:20 -0300

    config: adding first test route to project

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,3 +1,9 @@
 from flask import Flask
+from src.main.routes.trips_routes import trips_routes_bp
 
 app = Flask(__name__)
+
+app.register_blueprint(trips_routes_bp)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000, debug=True)
